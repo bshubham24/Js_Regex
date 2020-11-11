63f52262d3141d3f6a873b28c9e44bd546b4ce3d
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-11-11 14:18:01 +0530

    [Shubham]Added: Ability to verify incorrect pin which has alphabet at last

--- PinAndEmailValidation.js
+++ PinAndEmailValidation.js
@@ -1,5 +1,5 @@
 function pinValidator(pin){
-let pinRegex = RegExp('^[1-9]{1}[0-9]{5}$');
+let pinRegex = RegExp('^[1-9]{1}[0-9]{4}[0-9]{1}$');
 let checkPin = pinRegex.test(pin);
 if(checkPin){
     console.log(pin+" as pin is correct");
@@ -20,6 +20,11 @@ try {
 }
 try {
     pinValidator("A40008");
+} catch (error) {
+    console.error(error);
+}
+try {
+    pinValidator("A4000b");
 } catch (error) {
     console.error(error);
 }
